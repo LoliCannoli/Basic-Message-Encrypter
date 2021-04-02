083575f33831a65cb686d55742acd6b12fab7f18
Eva <camsboardprofile15@gmail.com>
2021-04-02 12:58:16 -0400

    Implimented working GUI

--- encyptAndDecrypt.py
+++ encyptAndDecrypt.py
@@ -5,25 +5,26 @@ class Message():
     def __init__(self, message):
         self.message = message
 
-    def encrypt(self, key):
+    def encryption(self, key):
         encodedMessage = self.message.encode()
-        encrypted = key.encrypt(message)
-        self.message = encrypted
+        encrypted = key.encrypt(encodedMessage)
+        self.message = str(encrypted).split('\'')[1]
 
-    def decrypt(self, key):
+
+    def decryption(self, key):
         try:
             decrypted = key.decrypt(self.message)
             decrypted = (str(decrypted)).split('\'')[1]
             self.message = decrypted
-        except InvalidToken:
+        except:
             print('Invalid decryption key provided')
 
-myHiddenMessage = 'This is a message'
-
-key = Fernet.generate_key()  # Store this key or get if you already have it
-f = Fernet(key)
-
-m = Message(myHiddenMessage)
-m.encrypt(f)
-m.decrypt(f)
-print(m.message)
+# myHiddenMessage = 'This is a message'
+#
+#key = Fernet.generate_key()
+# f = Fernet(key)
+#
+# m = Message(myHiddenMessage)
+# m.encrypt(f)
+# m.decrypt(f)
+# print(m.message)
--- makeTkinter.py
+++ makeTkinter.py
@@ -1,34 +1,70 @@
-#TODO: make buttons work
-#TODO: in the case of no ley input, generate key
-#TODO: or make no option to input key
-
 from tkinter import *
+from encyptAndDecrypt import Message
+from cryptography.fernet import Fernet
+import base64
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 def makeWindow():
-    window = tkinter.Tk()
+    window = Tk()
     window.resizable(0,0)
     window.geometry("675x350")
 
+    def Encrypt():
+
+        key = keyText.get(1.0, 'end').strip()
+        message = messageText.get(1.0, 'end').strip()
+
+        if key and message:
+            message = Message(message)
+            try:
+                key = Fernet(key.encode())
+
+            except base64.binascii.Error:
+                keyText.delete(1.0, 'end')
+                keyText.insert(1.0, 'Invalid Key')
+                return
+
+        message.encryption(key)
+
+        messageText.delete(1.0, 'end')
+        messageText.insert(1.0, message.message)
+
+    def Decrypt():
+        key = keyText.get(1.0, 'end').strip()
+        message = messageText.get(1.0, 'end').strip()
+
+        if key and message:
+            message = Message(message.encode())
+
+        try:
+            key = Fernet(key.encode())
+
+        except base64.binascii.Error:
+            keyText.delete(1.0, 'end')
+            keyText.insert(1.0, 'Invalid Key')
+            return
+
+        message.decryption(key)
+
+        messageText.delete(1.0, 'end')
+        messageText.insert(1.0, message.message)
+
     buttonFrame = Frame(window).pack(side = BOTTOM)
-    textFrame = Frame(window).pack(side = TOP)
+    inputFrame = Frame(window).pack(side = TOP)
 
+    messageTextLabel = Label(inputFrame, text = 'Your message:').pack()
+    messageText = Text(height = 7)
+    messageText.pack()
 
-    def makeButton(buttonText, buttonSide):
-        button = tkinter.Button(buttonFrame, text = buttonText, width = 40, height = 5).pack(side = buttonSide)
-        return button
+    keyTextLabel = Label(inputFrame, text = 'Your Key:').pack()
+    keyText = Text(height = 2)
+    keyText.pack()
 
-    def makeText(fieldName, fieldHeight):
-        label = tkinter.Label(textFrame, text = fieldName).pack()
-
-        entry = tkinter.Text(height = fieldHeight).pack()
-        return entry
-
-
-    messageText = makeText('Message:', 7)
-    keyText = makeText('Key:', 1)
-
-    encryptButton = makeButton('Encrypt', LEFT)
-    decryptButton = makeButton('Decrypt', RIGHT)
+    encryptButton = Button(buttonFrame, text = 'Encrypt', width = 40, height = 5, command = Encrypt).pack(side = LEFT)
+    decryptButton = Button(buttonFrame, text = 'Decrypt', width = 40, height = 5, command = Decrypt).pack(side = RIGHT)
 
     window.mainloop()
-    return messageText, keyText
+
+makeWindow()
